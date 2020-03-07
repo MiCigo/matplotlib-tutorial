dee9f99349b91b2264f6ee50bd8b38f379f4bf79
MiCigo <micihaha@aliyun.com>
2020-03-07 23:55:35 +0800

    create basic-3

---
+++ basic-3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+@Description: code
+@Author: MiCi
+@Date: 2020-03-07 23:38:05
+@LastEditTime: 2020-03-07 23:38:42
+@LastEditors: MiCi
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def basic_label_3():
+    return
+
+
+def basic_label_3_practice():
+    print('write your anwser')
+
+
+def basic_label_3_practice_answer():
+    return
+
+
+if __name__ == '__main__':
+    print('Start about basic label 3')
+    basic_label_3()
